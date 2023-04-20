457b0e3b2669094de0322eb6e26f699f6e24932b
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-04-20 09:33:58 +0200

    Images benaming duidelijker maken en png controle op hoofdletter gevoelig

--- index.php
+++ index.php
@@ -24,15 +24,11 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <!-- icon -->
+        
+    <!-- icon -->
         <?php $page = Pages::getHeader(); ?>
-        <?php if ($page == "" || $page == "home" || $page == "portaal" || $page == "quiz") : ?>
-            <link rel="icon" href="images/">
+            <link rel="icon" href="./images/LogoWebIcon.png">
             <link rel="stylesheet" href="style.css">
-        <?php else : ?>
-            <link rel="icon" href="../images/">
-            <link rel="stylesheet" href="../style.css">
-        <?php endif; ?>
         
         <title>UNESCO Guardians</title>
     </head>
--- pages/Portaal.php
+++ pages/Portaal.php
@@ -2,7 +2,7 @@
     <div class="container">
     <div class="row">
         <div class="col-sm-12">
-        <img src="images/" alt="">
+        <img src="./images/monkey.gif" height="1000px" width="500px">
         </div>
     </div>
     </div>
