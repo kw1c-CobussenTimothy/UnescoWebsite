2f7037f2fdca7f20c8e81b7ff90ad26f238df3bf
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-04-24 10:39:17 +0200

    lijnen compleet

--- pages/Portaal.php
+++ pages/Portaal.php
@@ -1,13 +1,19 @@
 <main>
     <div class="container">
     <div class="row">
-        <div class="col-12 col-lg-6 d-flex mx-5 justify-content-center align-items-center" style="margin-top: 45vh;">
+        <div class="col-12 col-lg-6 d-flex mx-5 justify-content-center align-items-center" style="margin-top: 38vh;">
         <div class="max-width: 646vh;">
             <img src="./images/animatie.gif" class="img-fluid">
         </div>
         </div>
         <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center">
-                <img src="./images/beginlijntest.png" class="my-custom-image-class">            
+                <img src="./images/beginlijn.png" class="beginlijn-class img-fluid">            
+        </div>
+        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center" style="margin-top: 131%;">
+                <img src="./images/heldenlijn.png" class="heldenlijn-class img-fluid">            
+        </div>
+        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center" style="margin-top: 131%;">
+                <img src="./images/eindlijn.png" class="heldenlijn-class img-fluid">            
         </div>
   </div>
 </main>
