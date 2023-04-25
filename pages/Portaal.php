f54364070e588209a606a9bfe645d07d75481928
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-04-25 09:23:19 +0200

    Afbeeldingen klaar plus tekst testen 1/3

--- pages/Portaal.php
+++ pages/Portaal.php
@@ -1,12 +1,24 @@
 <main>
-    <div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col-12 col-lg-6 d-flex mx-5 justify-content-center align-items-center" style="margin-top: 38vh;">
+        <div class="col-12 col-lg-6 d-flex mx-5 justify-content-center align-items-center" style="margin-top: 44vh;">
         <div class="max-width: 646vh;">
             <img src="./images/animatie.gif" class="img-fluid">
         </div>
         </div>
         <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center">
+        <p>
+            <h1 class="mt-5" style="position: absolute;" data-toggle="collapse" data-target="#loper">Loper</h1>
+                <div class="bsch-background" style="position: absolute; margin-top: 10%; margin-right: 50%;" id="loper" class="collapse">
+                    <h1>De loper</h1>
+                        <p class="txt-button">
+                            De loper mag meerdere velden schuin bewegen en slaan. <br> 
+                            Hij loopt dus over diagonalen. <br> 
+                            Een loper die op een zwart veld staat zal dus altijd op een zwart veld blijven, <br>
+                            hetzelfde geldt voor een loper op een wit veld.
+                        </p>                           
+                </div>
+        </p>
                 <img src="./images/beginlijn.png" class="beginlijn-class img-fluid">            
         </div>
         <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center" style="margin-top: 131%;">
@@ -14,6 +26,9 @@
         </div>
         <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center" style="margin-top: 131%;">
                 <img src="./images/eindlijn.png" class="heldenlijn-class img-fluid">            
+        </div>
+        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center" style="margin-top: 80%;">
+                <img src="./images/karakter_eind_groepspose.png" class="karakters-einde img-fluid">            
         </div>
   </div>
 </main>
