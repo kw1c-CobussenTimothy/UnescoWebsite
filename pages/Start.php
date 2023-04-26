df984ebb5287027047e14f75c2e42e84597ce276
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-04-26 09:30:24 +0200

    Start pagina klaar en responsive

--- pages/Start.php
+++ pages/Start.php
@@ -1,3 +1,26 @@
 <main>
-    hallo
+<div class="">
+  <div class="row">
+    <div class="">
+      <div id="image" onclick="showNextImage()">
+        <img src="images/portaal.png" id="portaal1" class="img-fluid portaal-class" style="position: absolute; border: none;">
+        <img src="images/overgang.png" id="overgang2" class="img-fluid overgang-class" style="display:none; border: none;">
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+    let currentImage = 1;
+    function showNextImage() {
+    if (currentImage == 1) {
+        document.getElementById("portaal1").style.display = "none";
+        document.getElementById("overgang2").style.display = "block";
+        currentImage = 2;
+        setTimeout(function() {
+        window.location.href = "portaal";
+        }, 1500); // Geef tijd aan in secondes voor de overgang
+    }
+    }
+</script>
 </main>
