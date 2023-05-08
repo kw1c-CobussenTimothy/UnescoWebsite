75b041a6ba1876cf6c8bf3a46cf27afc78a1763b
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-05-08 11:16:04 +0200

    code van vorige keer

--- pages/Start.php
+++ pages/Start.php
@@ -1,5 +1,5 @@
 <main>
-  <!--Start pagina, met behulp van de javascript code krijg je het effect dat je door het portaal-->
+  <!--Start pagina, met behulp van de javascript code krijg je het effect dat je door het portaal gaat-->
   <div class="row">
     <div class="">
       <div id="image" onclick="showNextImage()">
@@ -7,19 +7,24 @@
         <img src="images/overgang.png" id="overgang2" class="img-fluid overgang-class" style="display:none; border: none;">
       </div>
     </div>
-</div>
+  </div>
 
-<script>
+  <audio id="sound">
+    <source src="./audio/sparklin.mp3" type="audio/mpeg">
+  </audio>
+
+  <script>
     let currentImage = 1;
     function showNextImage() {
-    if (currentImage == 1) {
+      if (currentImage == 1) {
         document.getElementById("portaal1").style.display = "none";
         document.getElementById("overgang2").style.display = "block";
         currentImage = 2;
+        document.getElementById("sound").play(); // Speelt audio af
         setTimeout(function() {
-        window.location.href = "portaal";
+          window.location.href = "portaal";
         }, 1500); // Geef tijd aan in secondes voor de overgang
+      }
     }
-    }
-</script>
+  </script>
 </main>
