a10217ec66c2425218f73bb8392f8e4451c6955f
Yahye Mohamed <yahye.mohamed@rocdeleijgraaf.nl>
2023-05-10 15:39:56 +0200

    quiz ver 2.0

--- pages/Quiz.php
+++ pages/Quiz.php
@@ -1,146 +1,279 @@
 <?php
+//// Define the questions and their corresponding points for each category
+//$categories = [
+//    'Sports' => [
+//        [
+//            'question' => 'Which country won the FIFA World Cup in 2018?',
+//            'options' => ['France', 'Brazil', 'Germany', 'Argentina'],
+//            'correct' => 'France',
+//            'points' => 10
+//        ],
+//        [
+//            'question' => 'Which sport is associated with Wimbledon?',
+//            'options' => ['Tennis', 'Cricket', 'Football', 'Basketball'],
+//            'correct' => 'Tennis',
+//            'points' => 10
+//        ]
+//    ],
+//    'Geography' => [
+//        [
+//            'question' => 'What is the capital city of Australia?',
+//            'options' => ['Sydney', 'Melbourne', 'Canberra', 'Brisbane'],
+//            'correct' => 'Canberra',
+//            'points' => 10
+//        ],
+//        [
+//            'question' => 'Which country is home to the Great Barrier Reef?',
+//            'options' => ['Australia', 'Brazil', 'Mexico', 'India'],
+//            'correct' => 'Australia',
+//            'points' => 10
+//        ]
+//    ]
+//    // Add more categories and questions here...
+//];
+//
+//// Initialize the quiz session if it hasn't been started
+//if (!isset($_SESSION['quizStarted'])) {
+//    $_SESSION['quizStarted'] = true;
+//    $_SESSION['categoryPoints'] = array_fill_keys(array_keys($categories), 0);
+//    $_SESSION['totalPoints'] = 0;
+//}
+//
+//// Get the current category and question index
+//$categoryIndex = isset($_GET['c']) ? $_GET['c'] : 0;
+//$questionIndex = isset($_GET['q']) ? $_GET['q'] : 0;
+//
+//// Check if the quiz has ended
+//$quizEnded = ($categoryIndex >= count($categories));
+//
+//// Process the form submission if the quiz is still ongoing
+//if ($_SERVER['REQUEST_METHOD'] === 'POST' && !$quizEnded) {
+//    // Get the selected answer for the current question
+//    $selectedAnswer = $_POST['answer'];
+//
+//    // Get the current category, question, and the correct answer
+//    $category = array_keys($categories)[$categoryIndex];
+//    $question = array_keys($categories[$category])[$questionIndex];
+//    $correctAnswer = $categories[$category][$question]['correct'];
+//    $points = $categories[$category][$question]['points'];
+//
+//    // Check if the selected answer is correct
+//    if ($selectedAnswer === $correctAnswer) {
+//        $_SESSION['categoryPoints'][$category] += $points; // Increase category points for each correct answer
+//        $_SESSION['totalPoints'] += $points; // Increase total points for each correct answer
+//    }
+//
+//    // Redirect to the next question or show the result if there are no more questions
+//    if ($questionIndex + 1 < count($categories[$category])) {
+//        $nextQuestionIndex = $questionIndex + 1;
+//        header("Location: quiz?q=$nextQuestionIndex&c=$categoryIndex");
+//        exit();
+//    } elseif ($categoryIndex + 1 < count($categories)) {
+//        $nextCategoryIndex = $categoryIndex + 1;
+//        $nextQuestionIndex = 0;
+//        header("Location: quiz?q=$nextQuestionIndex&c=$nextCategoryIndex");
+//        exit();
+//    } else {
+//        // Quiz has ended
+//        $quizEnded = true;
+//    }
+//}
+//
+//// Retrieve the category points and total points from the session
+//$categoryPoints = $_SESSION['categoryPoints'];
+//$totalPoints = $_SESSION['totalPoints'];
+//
+//// Find the category with the highest points
+//$highestPoints = max($categoryPoints);
+//$winningCategory = array_search($highestPoints, $categoryPoints);
+//
+//// Reset the quiz session if the quiz has ended
+//if ($quizEnded) {
+//    unset($_SESSION['quizStarted']);
+//    unset($_SESSION['categoryPoints']);
+//    unset($_SESSION['totalPoints']);
+//}
+//?>
+<!---->
+<!--    <!-- Quiz form -->-->
+<!--    <!DOCTYPE html>-->
+<!--    <html>-->
+<!--    <head>-->
+<!--        <title>Quiz System</title>-->
+<!--    </head>-->
+<!--<body>-->
+<?php //if ($quizEnded): ?>
+<!--    <h3>Quiz Ended</h3>-->
+<!--    <h4>Category Points:</h4>-->
+<!--    <ul>-->
+<!--        --><?php //foreach ($categoryPoints as $category => $points): ?>
+<!--            <li>--><?php //echo $category; ?><!--: --><?php //echo $points; ?><!--</li>-->
+<!--        --><?php //endforeach; ?>
+<!--    </ul>-->
+<!--    --><?php //if ($winningCategory): ?>
+<!--        <h4>Winner: --><?php //echo $winningCategory; ?><!--</h4>-->
+<!--        <!-- Add your specific text for the winner here -->-->
+<!--        --><?php //if ($winningCategory === 'Sports'): ?>
+<!--            <p>Congratulations to the Sports category winner!</p>-->
+<!--        --><?php //else: ?>
+<!--            <form method="post" action="">-->
+<!--                --><?php
+//                // Get the current category, question, and options
+//                $category = array_keys($categories)[$categoryIndex];
+//                $question = array_keys($categories[$category])[$questionIndex];
+//                $options = $categories[$category][$question]['options'];
+//
+//                // Display the current category and question
+//                echo "<h2>$category</h2>";
+//                echo "<p>$question</p>";
+//
+//                // Generate radio buttons for the question options
+//                foreach ($options as $option) {
+//                    echo "<input type='radio' name='answer' value='$option'> $option<br>";
+//                }
+//                ?>
+<!---->
+<!--                <input type="submit" value="Next">-->
+<!--            </form>-->
+<!--        --><?php //endif; ?>
+<!--    --><?php //endif; ?>
+<?php //endif; ?>
+<!--</body>-->
+<!--</html>-->
 
-$pointSystems = array(
-1 => array(
-'questions' => array(
-1 => array(
-'question' => 'Hoe vaak ga je naar een kringloopwinkel per jaar?',
-'answers' => array(
-'0 keer' => 0,
-'1-3 keer' => 5,
-'4-8 keer' => 10,
-'9+ keer' => 15
-)
-),
-2 => array(
-'question' => 'Hoe vaak per maand koop je wel eens iets online?',
-'answers' => array(
-'0 keer' => 0,
-'1-2 keer' => 5,
-'3-4 keer' => 10,
-'5+ keer' => 15
-)
-)
-),
-'points' => 0
-),
-2 => array(
-'questions' => array(
-3 => array(
-'question' => 'Vind je dat iedereen de zelfde rechten moet krijgen?',
-'answers' => array(
-'Zeker!' => 15,
-'Ja, maar ze moeten wel hun best er voor doen' => 10,
-'Nee, maar sommige mensen wel' => 5,
-'Nee natuurlijk niet' => 0
-)
-),
-4 => array(
-'question' => 'Wat doe je met eten wat je te veel hebt gemaakt?',
-'answers' => array(
-'Ik gooi het in de afvalbak' => 0,
-'Ik gooi het in de groenbak' => 5,
-'Ik vries het in voor later' => 10,
-'Ik geef het weg aan een ander' => 15
-)
-)
-),
-'points' => 0
-),
-3 => array(
-'questions' => array(
-5 => array(
-'question' => 'Hoeveel ben jij bezig met andere culturen?',
-'answers' => array(
-'Helemaal niet' => 0,
-'Wel een beetje' => 5,
-'Ik weet best veel' => 10,
-'Ik houd me er veel mee bezig' => 15
-)
-),
-6 => array(
-'question' => 'Ik maak makkelijk nieuwe vrienden',
-'answers' => array(
-'Nee bedankt' => 0,
-'Nee, maar ik zou het wel willen' => 5,
-'Denk het wel' => 10,
-'Zekers!' => 15
-)
-)
-),
-'points' => 0
-),
-4 => array(
-'questions' => array(
-7 => array(
-    'question' => 'Wat zou jij doen als je te weten krijgt dat een docent een student die hij/zij minder aardig vind, een lager punt geeft als andere zonder reden?',
-'answers' => array(
-'Ik zou het stom vinden maar er niks aan doen' => 0,
-'Ik zou er met de docent over praten' => 5,
-'Ik zou er met de schoolleiding over praten' => 10,
-'Ik zou er met de studentenraad over praten' => 15
-)
-),
-8 => array(
-'question' => 'Ben jij bereid om iets extras te doen voor het milieu?',
-'answers' => array(
-'Nee, ik vind het niet belangrijk' => 0,
-'Ja, als het niet te veel moeite kost' => 5,
-'Ja, ik doe best veel' => 10,
-'Ja, ik ben er echt actief mee bezig' => 15
-)
-)
-),
-'points' => 0
-)
-);
+<?php
+// Define the questions and their corresponding points for each category
+$categories = [
+    'Sports' => [
+        [
+            'question' => 'Which country won the FIFA World Cup in 2018?',
+            'options' => ['France', 'Brazil', 'Germany', 'Argentina'],
+            'correct' => 'France',
+            'points' => 10
+        ],
+        [
+            'question' => 'Which sport is associated with Wimbledon?',
+            'options' => ['Tennis', 'Cricket', 'Football', 'Basketball'],
+            'correct' => 'Tennis',
+            'points' => 10
+        ]
+    ],
+    'Geography' => [
+        [
+            'question' => 'What is the capital city of Australia?',
+            'options' => ['Sydney', 'Melbourne', 'Canberra', 'Brisbane'],
+            'correct' => 'Canberra',
+            'points' => 10
+        ],
+        [
+            'question' => 'Which country is home to the Great Barrier Reef?',
+            'options' => ['Australia', 'Brazil', 'Mexico', 'India'],
+            'correct' => 'Australia',
+            'points' => 10
+        ]
+    ]
+    // Add more categories and questions here...
+];
 
-$points = isset($_SESSION['points']) ? $_SESSION['points'] : 0;
-$currentQuestion = isset($_SESSION['currentQuestion']) ? $_SESSION['currentQuestion'] : 1;
-$currentPointSystem = isset($_SESSION['currentPointSystem']) ? $_SESSION['currentPointSystem'] : 1;
-$currentQuestion = max(1, min(count($pointSystems[$currentPointSystem]['questions']), $currentQuestion));
-
-if(isset($_POST['answer'])) {
-    $answer = (int)$_POST['answer'];
-    $points += $pointSystems[$currentPointSystem]['questions'][$currentQuestion]['answers'][$answer];
-    $_SESSION['points'] = $points;
-    $currentQuestion++;
-    $_SESSION['currentQuestion'] = $currentQuestion;
+// Initialize the quiz session if it hasn't been started
+if (!isset($_SESSION['quizStarted'])) {
+    $_SESSION['quizStarted'] = true;
+    $_SESSION['categoryPoints'] = array_fill_keys(array_keys($categories), 0);
+    $_SESSION['totalPoints'] = 0; // Initialize total points to 0
 }
 
-if($currentQuestion > count($pointSystems[$currentPointSystem]['questions'])) {
-    $pointSystems[$currentPointSystem]['points'] = $points;
-    $currentPointSystem++;
-    $currentQuestion = 1;
-    $_SESSION['currentQuestion'] = $currentQuestion;
+// Get the current category and question index
+$categoryIndex = isset($_GET['c']) ? $_GET['c'] : 0;
+$questionIndex = isset($_GET['q']) ? $_GET['q'] : 0;
 
-    // Add this check to ensure that the current point system is within bounds
-    if ($currentPointSystem <= count($pointSystems)) {
-        $_SESSION['currentPointSystem'] = $currentPointSystem;
+// Check if the quiz has ended
+$quizEnded = ($categoryIndex >= count($categories));
+
+// Process the form submission if the quiz is still ongoing
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !$quizEnded) {
+    // Get the selected answer for the current question
+    $selectedAnswer = $_POST['answer'];
+
+    // Get the current category, question, and the correct answer
+    $category = array_keys($categories)[$categoryIndex];
+    $question = $categories[$category][$questionIndex]['question'];
+    $correctAnswer = $categories[$category][$questionIndex]['correct'];
+    $points = $categories[$category][$questionIndex]['points'];
+
+    // Check if the selected answer is correct
+    if ($selectedAnswer === $correctAnswer) {
+        $_SESSION['categoryPoints'][$category] += $points; // Increase category points for each correct answer
+        $_SESSION['totalPoints'] += $points; // Increase total points for each correct answer
+    }
+
+    // Redirect to the next question or show the result if there are no more questions
+    if ($questionIndex + 1 < count($categories[$category])) {
+        $nextQuestionIndex = $questionIndex + 1;
+        header("Location: quiz?q=$nextQuestionIndex&c=$categoryIndex");
+        exit();
+    } elseif ($categoryIndex + 1 < count($categories)) {
+        $nextCategoryIndex = $categoryIndex + 1;
+        $nextQuestionIndex = 0;
+        header("Location: quiz?q=$nextQuestionIndex&c=$nextCategoryIndex");
+        exit();
+    } else {
+        // Quiz has ended
+        $quizEnded = true;
     }
 }
 
-if ($currentPointSystem > count($pointSystems)) {
-    echo 'Je hebt alle vragen beantwoord!<br>';
-    echo 'Hier zijn de punten van elk systeem:<br>';
+// Retrieve the category points from the session
+$categoryPoints = $_SESSION['categoryPoints'];
+$totalPoints = $_SESSION['totalPoints'];
 
-    foreach($pointSystems as $key => $system) {
-        echo 'Systeem ' . $key . ': ' . $system['points'] . '<br>';
-    }
-} else {
-    if (isset($pointSystems[$currentPointSystem]['questions'][$currentQuestion])) {
-        $question = $pointSystems[$currentPointSystem]['questions'][$currentQuestion]['question'];
-    }
-    else {
-        echo 'Sorry er zijn niet meer vragen';
-    }
-$answers = $pointSystems[$currentPointSystem]['questions'][$currentQuestion]['answers'];
-echo '<p>' . $question . '</p>';
-echo '<form method="post">';
-    foreach($answers as $key => $value) {
-    echo '<input type="radio" name="answer" value="' . $key . '">' . $key . '<br>';
-    }
-    echo '<br><input type="submit" value="Volgende vraag">';
-    echo '</form>';
-}
-session_destroy();
+// Find the category with the highest points
+$highestPoints = max($categoryPoints);
+$winningCategory = array_search($highestPoints, $categoryPoints);
+
+var_dump($categoryPoints)   ;
+var_dump($winningCategory);
+////if welke cateorie de meeste punten eeft
+//
+//
+$_SESSION["held"] = $winningCategory;
+?>
+
+<!-- Quiz form -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz System</title>
+</head>
+<body>
+<?php if ($quizEnded): ?>
+    <h3>Quiz Ended</h3>
+    <h4>Category Points:</h4>
+    <ul>
+        <?php foreach ($categoryPoints as $category => $points): ?>
+            <li><?php echo $category; ?>: <?php echo $points; ?></li>
+        <?php endforeach; ?>
+    </ul>
+    <h4>Total Points: <?php echo $totalPoints; ?></h4>
+<?php else: ?>
+    <form method="post" action="">
+        <?php
+        // Get the current category, question, and options
+        $category = array_keys($categories)[$categoryIndex];
+        $question = $categories[$category][$questionIndex]['question'];
+        $options = $categories[$category][$questionIndex]['options'];
+
+        // Display the current category and question
+        echo "<h2>$category</h2>";
+        echo "<p>$question</p>";
+
+        // Generate radio buttons for the question options
+        foreach ($options as $option) {
+            echo "<input type='radio' name='answer' value='$option'> $option<br>";
+        }
+        ?>
+
+        <input type="submit" value="Next">
+    </form>
+<?php endif; ?>
+</body>
+</html>
