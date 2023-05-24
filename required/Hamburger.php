d5e6a84714a34f794adc69247a32e1a3163b9ecd
kw1c-CobussenTimothy <78853852+kw1c-CobussenTimothy@users.noreply.github.com>
2023-05-24 13:59:57 +0200

    Hamburger menu commentaar

--- required/Hamburger.php
+++ required/Hamburger.php
@@ -1,20 +1,28 @@
+<!-- Zijmenu met daarin de links naar andere pagina's -->
 <div id="NavBar" class="sidenav">
-					<a href="javascript:void(0)" id="closebutton" onclick="Close()">&times;</a>
-					<a href="/UnescoWebsite/index">Hoofdpagina</a>
-					<a href="/UnescoWebsite/pages/Quiz">Quiz</a>
-					<a href="https://sdgs.un.org/goals" target="_blank">Unesco klimaatdoelen</a>
-					<a href="/UnescoWebsite/pages/About">Over ons</a>
-				</div>
-<span style="font-size: 30px;cursor:pointer;" id="Container" onclick="Open()">&#9776;</span>
+	<!-- Knop om het menu af te sluiten -->
+	<a href="javascript:void(0)" id="closebutton" onclick="Close()">&times;</a>
+
+	<!-- Verwijzingen naar de andere pagina's / sites -->
+	<a href="/UnescoWebsite/index">Hoofdpagina</a>
+	<a href="/UnescoWebsite/pages/Quiz">Ontdek je Innerlijke Held</a>
+	<a href="https://sdgs.un.org/goals" target="_blank">Unesco klimaatdoelen</a>
+	<a href="/UnescoWebsite/pages/Heroes">Helden</a>
+	<a href="/UnescoWebsite/pages/About">De makers</a>
+</div>
+
+<!-- Knop voor het navigatie menu -->
+<span style="font-size: 40px;cursor:pointer;" id="Container" onclick="Open()">&#9776;</span>
 
 
 <script>
+// gives the sidebar a width of 45, thus putting it in view
 function Open()
   {
     document.getElementById("NavBar").style.width = "45%";
   }
 
-  // gives the sidebar a width of 0, thus removing it from view
+// gives the sidebar a width of 0, thus removing it from view
   function Close()
   {
     document.getElementById("NavBar").style.width = "0%";
@@ -22,7 +30,12 @@ function Open()
 </script>
 
 <style>
+body{
+    background-color: #180035;
+    color: white;
+}
 
+/* Het navigatie menu zelf */
 .sidenav
 {
 	height: 100%;
@@ -30,27 +43,30 @@ function Open()
 	position: fixed;
 	top: 0px;
 	left: 0px;
-	background-color: #000000;
+	background-color: #280059;
 	overflow-x: hidden;
-	transition: 0.5s;
+	transition: 0.25s;
 	padding-top: 60px;
 	z-index: 1;
 }
-
+/* De text binnen het navigatiemenu */
 .sidenav a
 {
 	padding: 8px 8px 8px 32px;
 	text-decoration: none;
 	display: block;
-	transition: 0.3s;
-	font-size: 17px;
+	transition: 0.25s;
+	font-size: 21px;
+	color: #ffffff;
 }
 
+/* De knop om het menu te sluiten */
 .sidenav #closebutton
 {
 	position: absolute;
 	top: 40px;
 	right: 25px;
 	margin-right: 0;
+	font-size: 40px;
 }
-</style>
+</style> 
