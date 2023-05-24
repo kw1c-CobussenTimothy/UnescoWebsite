b8e5e42e8c3d9e8839ea70790423cff2a1ff75fe
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-05-24 15:27:29 +0200

    css enzo

--- pages/RaeveHeld.php
+++ pages/RaeveHeld.php
@@ -1,20 +1,20 @@
 <main>
     <div class="col-12">        
-        <div class="card" style="position: absolute; margin-left: 55%; margin-top: 80%;">
+        <div class="card" style="position: absolute; margin-left: 58%; margin-top: 80%;">
             test test test
         </div> 
         <div class="row col-12">
             <h1 style="position:absolute; margin-top: 15%; margin-left: 10%;" class="guardian-tekst2"><strong>Duurzaamheid</strong></h1>
             <h1 style="position:absolute; margin-top: 22%; margin-left: 10%;" class="guardian-tekst2"><strong><i>Vera/Raeve</i></strong></h1>
             <div class="col-8 d-flex justify-content-center align-items-center">
-                <img class="raeve"  src="./images/raeve.png" style=" margin-top: 100%; margin-right: 30%;">
+                <img class="raeve"  src="./images/raeve.png" style=" margin-top: 120%; margin-right: 25%;">
             </div>
             <div class="col-4">
                 <img class="banner" src="./images/bannergroen.png">
             </div>
         </div>
         <div class="col-12 d-flex justify-content-center align-items-center">              
-            <img class="rotate" src="./images/rotate.png" style="margin-right: 50%; margin-top: 15%;">    
+            <img class="rotate" src="./images/rotate.png" style="margin-right: 50%; margin-top: 20%;">    
         </div>
     </div>
 </main>
