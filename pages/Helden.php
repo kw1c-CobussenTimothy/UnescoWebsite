ab58eae8f7116c32ff5cf94c4b3650523a9b4abb
kw1c-CobussenTimothy <78853852+kw1c-CobussenTimothy@users.noreply.github.com>
2023-05-25 14:58:55 +0200

    Helden pagina spinny images

--- pages/Helden.php
+++ pages/Helden.php
@@ -2,7 +2,9 @@
     <body>
         <div class="container col-12">
             <div class="row">
-                <div class="col-6 d-flex justify-content-center align-items-center"><img class="image2 px-4 py-3" src="/UnescoWebsite/images/Vera.png"></div>
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <img id="VeraImage" class="image2 px-4 py-3" src="/UnescoWebsite/images/Vera.png"> 
+                </div>
                 <div class="col-6 center-block fs-5"><br>
                     <p>Dit is Vera. Zij studeert mediavormgeving bij ICT & Creatief. Daarnaast heeft ze een bijbaan in een restaurant. Er wordt daar vaak eten weggegooid Porties zijn te groot en mensen kunnen hun maaltijd niet op.  Op een avond serveert Vera een ouder echtpaar en vraagt of alles naar wens was. 
                     </p>
@@ -15,8 +17,11 @@
                     <p>Zo vermindert Raeve voedselverspilling. Het echtpaar, nog steeds onder de indruk van wat er voor hun ogen gebeurde neemt met een glimlach en een goedgevulde doggybag afscheid.
                     </p>
                 </div>
+                <div class="col-12 mt-1">
+                    <button class="col-4" style="margin-left: 8.333%;" onclick="Spinny()">switch</button> 
+                </div>
             </div>
-
+            
             <div class="row">
                 <div class="col-6 center-block fs-5"><br>
                     <p>Taiana  komt uit Kiev en leert voor Allround Constructiewerker. Zij is statushouder en omdat ze moest vluchten heeft ze alles achter moeten laten. Sommige van haar vrienden zijn gevlucht anderen zijn daar gebleven. De beelden in het nieuws laten zien dat het wel jaren kan duren voordat alles wat is verwoest is hersteld.
@@ -25,13 +30,19 @@
                     </p>
                     <p>Tatiana is veranderd in Tianat die strijd voor vrede en mensenrechten</p>
                 </div>
-                <div class="col-6 d-flex justify-content-center align-items-center"><img class="image2 px-4 py-3" src="/UnescoWebsite/images/Tatiana.png"></div>
+                <div class="col-6 d-flex justify-content-center align-items-center"><img id="TatianaImage" class="image2 px-4 py-3" src="/UnescoWebsite/images/Tatiana.png"></div>
+                <div class="col-12 mt-1">
+                    <button class="col-4" style="margin-left: 58.333%;" onclick="Spinny2()">switch</button> 
+                </div>
             </div>
 
             <div class="row">
-                <div class="col-6 d-flex justify-content-center align-items-center"><img class="image2 px-4 py-3" src="/UnescoWebsite/images/Rob.png"></div>
+                <div class="col-6 d-flex justify-content-center align-items-center"><img id="RobImage" class="image2 px-4 py-3" src="/UnescoWebsite/images/Rob.png"></div>
                 <div class="col-6 center-block fs-5"><br>
                     <p>Dit is Rob, student Leidinggevende travel en hospitality. Tijdens de lunchpauze op school ziet hij dat een klasgenoot wordt gepest vanwege zijn Aziatische afkomst. Rob heeft een hekel aan discriminatie en buitensluiten hij springt daarom in actie. Wanneer Rob de pestkop aan wil spreken op zijn gedrag voelt Rob een krachtig gevoel in zijn borst opwellen en ziet een fel licht om zich heen. Wanneer hij de pester aanraakt bij zijn schouder blijkt Rob te zijn veranderd in Bohr, een superheld met teleportatiekrachten. Op deze manier neemt hij de pestkop mee naar een andere cultuur en kan zo laten zien hoe mooi en interessant andere culturen zijn. Bohr laat de pester een tijd achter in Japan. Daarna teleporteert Bohr de jongen terug. Na deze ervaring blijkt hij meer begrip te hebben voor mensen met een andere culturele achtergrond dan de zijne. </p>
+                </div>
+                <div class="col-12 mt-1">
+                    <button class="col-4" style="margin-left: 8.333%;" onclick="Spinny3()">switch</button> 
                 </div>
             </div>
 
@@ -39,10 +50,80 @@
                 <div class="col-6 center-block fs-5"><br>
                     <p>Emily zit in het eerste jaar van de koksopleiding. Ze houdt ervan mensen blij te maken met het heerlijkste eten. Ze kookt al vanaf kleins af aan voor haar familie. Ze waren heel hecht als gezin nu zij en haar broer ouder worden merkt Emily ze dat er meer afstand ontstaat in haar gezin. Na de zoveelste ruzie gaat Emily verdrietig naar haar kamer ze zou willen dat ze met haar kookkunst de ruzies kon oplossen. Ineens verblindt een fel licht wat de kamer binnen schijnt haar zicht en voelt Emily iets in zichzelf opborrelen. Ze loopt naar de keuken en tovert binnen 2 minuten een feestmaal wat bestaat uit alle lievelingsgerechten van haar familie. Haar ouders en broer staan versteld, ze gaan samen aan tafel en onder het eten praten ze hun conflicten uit. Als superheld heet ze Limey degene die mensen bij elkaar brengt en verbindt waar dat hard nodig is. </p>
                 </div>
-                <div class="col-6 d-flex justify-content-center align-items-center"><img class="image2 px-4 py-3" src="/UnescoWebsite/images/Emily.png"></div>
+                <div class="col-6 d-flex justify-content-center align-items-center"><img id="EmilyImage" class="image2 px-4 py-3" src="/UnescoWebsite/images/Emily.png"></div>
+                <div class="col-12 mt-1">
+                    <button class="col-4" style="margin-left: 58.333%;" onclick="Spinny4()">switch</button> 
+                </div>
             </div>
         </div>
     </body>
 </html>
 
 
+<style>
+.hidden  {
+    display: none;
+}
+
+.spinny{
+    animation: spinY 2.5s ease-in 1s;
+} 
+.spinny2{
+    animation: spinY 2.5s ease-out 1.5s;
+} 
+
+@keyframes spinY {
+    0% {
+        transform: rotateY(0deg);
+    }
+    100% {
+        transform: rotateY(1800deg);
+    }
+}
+</style>
+
+<script>
+function Spinny() {
+    var Image1 = document.getElementById("VeraImage");
+    
+    Image1.classList.add("spinny");
+    setTimeout(() => {
+        Image1.src="/UnescoWebsite/images/Raeve.png";
+        Image1.classList.remove("spinny");
+        Image1.classList.add("spinny2");
+    }, 2500);
+}
+
+function Spinny2() {
+    var Image1 = document.getElementById("TatianaImage");
+    
+    Image1.classList.add("spinny");
+    setTimeout(() => {
+        Image1.src="/UnescoWebsite/images/Tianat.png";
+        Image1.classList.remove("spinny");
+        Image1.classList.add("spinny2");
+    }, 2500);
+}
+
+function Spinny3() {
+    var Image1 = document.getElementById("RobImage");
+    
+    Image1.classList.add("spinny");
+    setTimeout(() => {
+        Image1.src="/UnescoWebsite/images/Bohr.png";
+        Image1.classList.remove("spinny");
+        Image1.classList.add("spinny2");
+    }, 2500);
+}
+
+function Spinny4() {
+    var Image1 = document.getElementById("EmilyImage");
+    
+    Image1.classList.add("spinny");
+    setTimeout(() => {
+        Image1.src="/UnescoWebsite/images/Limey.png";
+        Image1.classList.remove("spinny");
+        Image1.classList.add("spinny2");
+    }, 2500);
+}
+</script>
