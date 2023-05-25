5b76e94f913926d1e1ade4bbb94742eab80aa804
Berend305 <74530848+Berend305@users.noreply.github.com>
2023-05-25 09:29:30 +0200

    zooi van gisteren

--- pages/RaeveHeld.php
+++ pages/RaeveHeld.php
@@ -1,20 +1,20 @@
 <main>
     <div class="col-12">        
-        <div class="card" style="position: absolute; margin-left: 58%; margin-top: 80%;">
+        <div class="card" style="position: absolute; margin-left: 65%; margin-top: 100%;">
             test test test
         </div> 
         <div class="row col-12">
             <h1 style="position:absolute; margin-top: 15%; margin-left: 10%;" class="guardian-tekst2"><strong>Duurzaamheid</strong></h1>
             <h1 style="position:absolute; margin-top: 22%; margin-left: 10%;" class="guardian-tekst2"><strong><i>Vera/Raeve</i></strong></h1>
             <div class="col-8 d-flex justify-content-center align-items-center">
-                <img class="raeve"  src="./images/raeve.png" style=" margin-top: 120%; margin-right: 25%;">
+                <img class="raeve"  src="./images/raeve.png" style=" margin-top: 140%; margin-right: 25%;">
             </div>
             <div class="col-4">
-                <img class="banner" src="./images/bannergroen.png">
+                <img style="margin-left: 50%;" class="banner" src="./images/bannergroen.png">
             </div>
         </div>
         <div class="col-12 d-flex justify-content-center align-items-center">              
-            <img class="rotate" src="./images/rotate.png" style="margin-right: 50%; margin-top: 20%;">    
+            <img class="rotate" src="./images/rotate.png" style="margin-right: 50%; margin-top: 30%;">    
         </div>
     </div>
 </main>
