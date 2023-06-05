43d6ae250174901e68d94ea39d8888a5b989dec8
Jason Passeau <jason.passeau@edu-kw1c.nl>
2023-06-05 10:18:08 +0200

    Fontsize aangepast

--- pages/About.php
+++ pages/About.php
@@ -7,14 +7,14 @@
             <div class="col-6 center-block"><br> <p style="font-size:100px;">De makers</p>
         <div class="row">
             <div class="col-6"><img class="image1" src="./images/PortretFay.png"></div>
-            <div class="col-6 center-block"><br> <p>Hoi! Ik ben Fay Kooijmans, ik ben 17 jaar en studeer mediavormgeving 
+            <div class="col-6 center-block"><br> <p style="font-size:20px;">Hoi! Ik ben Fay Kooijmans, ik ben 17 jaar en studeer mediavormgeving 
                 op het KW1C en ik heb stage gelopen bij het Productiehis. Aan dit project heb ik de meeste tekeningen bijgedragen. 
                 De karakters, banners, het logo etc. Ook heb ik samen met Jesse het concept, werking en ontwerp van de app bedacht. 
 </p></div>
 </div>
 
         <div class="row">
-            <div class="col-6 center-block"><br> <p>Hallo! Mijn naam is Jesse Gerritsen, ik ben een 21 jarige student mediavormgeving 
+            <div class="col-6 center-block"><br> <p style="font-size:20px;">Hallo! Mijn naam is Jesse Gerritsen, ik ben een 21 jarige student mediavormgeving 
                 op het KW1C en heb stage gelopen bij het Productiehuis. Voor de app heb ik een aantal tekeningen gemaakt, waaronder ideeën 
                 voor het achtergrond en de lijn die te zien is, ook heb ik grotendeels de opmaak en indeling van de app bedacht. Ook heb ik 
                 samen met Fay het concept, de eerste karakter schetsen (de prototypes) en de werking van de app bedacht.</p></div>
@@ -23,13 +23,13 @@
 
         <div class="row">
             <div class="col-6"><img class="image1" src="./images/PortretJason.png"></div>
-            <div class="col-6 center-block"><br> <p>Heyo! Mijn naam is Jason Passeau, ik ben een 21 jarige student 
+            <div class="col-6 center-block"><br> <p style="font-size:20px;">Heyo! Mijn naam is Jason Passeau, ik ben een 21 jarige student 
                 Software Development op het KW1C in het derde jaar, en ik heb stage gelopen bij het productiehuis. Voor de web-app heb 
                 ik deze pagina en de heldenpagina gemaakt.</p></div>
         </div>
 
         <div class="row">
-            <div class="col-6 center-block"><br> <p>Hey! Mijn naam is Yahye Mohamed, ik ben een 19 jarige student Software Development 
+            <div class="col-6 center-block"><br> <p style="font-size:20px;">Hey! Mijn naam is Yahye Mohamed, ik ben een 19 jarige student Software Development 
                 op het KW1C en ik heb mijn eind-stage gelopen bij het Productiehuis. Voor dit project heb ik veel code geschreven, bijvoorbeeld 
                 voor het quiz systeem. Ik heb ook veel lopen mee denken aan manieren waarop we de app zo gebruikersvriendelijk en 
                 aantrekkelijk mogelijk konden maken.</p></div>
@@ -38,14 +38,14 @@
 
         <div class="row">
             <div class="col-6"><img class="image1" src="./images/PortretBerend.png"></div>
-            <div class="col-6 center-block"><br> <p>Hallo mensen! Mijn naam is Berend Beerendonk, ik ben een 21 jarige student 
+            <div class="col-6 center-block"><br> <p style="font-size:20px;">Hallo mensen! Mijn naam is Berend Beerendonk, ik ben een 21 jarige student 
                 Software Development op het KW1C en ik heb mijn eind-stage gelopen bij het Productiehuis. Ik heb voor dit project
                  veel gepland en code geschreven, ik was projectleider voor de groep programmeurs. Ik was verantwoordelijk voor het 
                  start pagina, portaal en helden pagina, wat ik het leuks vond was het maken van de animatie voor de pagina.</p></div>
         </div>
 
         <div class="row">
-            <div class="col-6 center-block"><br> <p>Hallo! Mijn naam is Timothy Cobussen, ik ben 20 jaar en volg de opleiding 
+            <div class="col-6 center-block"><br> <p style="font-size:20px;">Hallo! Mijn naam is Timothy Cobussen, ik ben 20 jaar en volg de opleiding 
                 Software Developer aan het KW1C. Bij het productiehuis hebben wij gewerkt aan deze mooie unesco belevenis, 
                 mijn rol hierbij was het creeeren van het menu en de navigatie over de hele website.</p></div>
             <div class="col-6"><img class="image1" src="./images/PortretTimothy.png"></div>
